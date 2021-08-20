4efaf41ad5f04f9ebcb8caebf6255a9dd5f408d2
adhityaranggap <cloudsample6@gmail.com>
2021-08-20 23:35:06 +0700

    learn basic next js

---
+++ pages/users/index.tsx
@@ -0,0 +1,37 @@
+import { useRouter } from 'next/router';
+import Layout from '../../components/Layout';
+import styles from '../../styles/Users.module.css';
+
+interface UsersProps {
+  dataUsers: Array<any>;
+}
+export default function User(props: UsersProps) {
+  const { dataUsers } = props;
+  const router = useRouter();
+  console.log(dataUsers);
+  return (
+    <Layout pageTitle="User Page">
+      {dataUsers.map((user) => (
+        <div
+          key={user.id}
+          onClick={() => router.push(`/users/${user.id}`)}
+          className={styles.card}
+        >
+          <p className="tittle">User Page</p>
+          <p>{user.name}</p>
+          <p>{user.email}</p>
+        </div>
+      ))}
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const dataUsers = await res.json();
+  return {
+    props: {
+      dataUsers,
+    },
+  };
+}
